e103e95d24cf7bf95358eec4c4e8227232b8a071
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2024-03-22 12:34:51 +0800

    update

--- components/about.tsx
+++ components/about.tsx
@@ -9,19 +9,20 @@ export const About = () => {
   return (
     <section
       ref={ref}
-      id="about"
-      className="max-w-xl leading-7 mb-28 sm:mb-40 scroll-mt-28 p-3"
+      id='about'
+      className='max-w-xl leading-7 mb-28 sm:mb-40 scroll-mt-28 p-3'
     >
       <MotionDiv delay={0.175}>
-        <SectionHeading title="About me" />
-        <p className="text-center text-neutral-600 text-xl dark:text-white/60">
+        <SectionHeading title='About me' />
+        <p className='text-center text-neutral-600 text-xl dark:text-white/60'>
           After graduating with a degree of Bachelor of science in Information
-          Technology, I work in a Real estate company as a web developer. A year
-          after, I transitioned into another company where i worked as a
-          fullstack developer, and a wondows application developer for 3 years.
-          It help me improved my problem solving and programming skills. At the
-          time being I am focused on learning the new web technologies such as
-          react, and nextjs.
+          Technology, I work in a Real estate company as a web developer where I
+          help the company in building the internet precense on top of the
+          google search. A year after, I transitioned into another company where
+          i worked as a fullstack developer working on windows application
+          development and website development for Research Journal for 3 years,
+          where I build web presence and was first to established the online
+          research submission which greatly help the university.
         </p>
       </MotionDiv>
     </section>
--- components/intro.tsx
+++ components/intro.tsx
@@ -12,73 +12,74 @@ export const Intro = () => {
   const { setIsActive, setTimeOfLastClick } = useActiveSection();
 
   return (
-    <section ref={ref} id="home" className="scroll-mt-36 p-3">
-      <div className="flex items-center justify-center">
-        <div className="relative">
-          <MotionDivImage type="tween">
+    <section ref={ref} id='home' className='scroll-mt-36 p-3'>
+      <div className='flex items-center justify-center'>
+        <div className='relative'>
+          <MotionDivImage type='tween'>
             <Image
-              src="/1x1.png"
-              alt="Profile image"
-              width="192"
-              height="192"
-              quality="95"
+              src='/1x1.png'
+              alt='Profile image'
+              width='192'
+              height='192'
+              quality='95'
               priority
-              className="h-24 w-24 rounded-full border-[0.34rem] object-cover border-white"
+              className='h-24 w-24 rounded-full border-[0.34rem] object-cover border-white'
             />
           </MotionDivImage>
           <MotionDivImage
-            type="spring"
+            type='spring'
             delay={0.1}
             duration={0.7}
             stiffness={125}
           >
-            <span className="text-4xl absolute right-0 bottom-0">👋</span>
+            <span className='text-4xl absolute right-0 bottom-0'>👋</span>
           </MotionDivImage>
         </div>
       </div>
-      <MotionDiv className="flex flex-col items-center justify-center mt-5">
-        <h1 className="text-3xl text-center">
+      <MotionDiv className='flex flex-col items-center justify-center mt-5'>
+        <h1 className='text-3xl text-center'>
           Hello my name is <strong>Almujahid Jamion</strong>
         </h1>
-        <p className="text-2xl text-center max-w-lg text-gray-700 leading-[1.3] dark:text-white/60">
-          I am a Fullstack developer with 5 years of experience. I enjoy Web
-          Apps. My focus is React (NextJs)
+        <p className='text-2xl text-center max-w-lg text-gray-700 leading-[1.3] dark:text-white/60'>
+          I am a Fullstack developer with 4 years of experience in web
+          development. I always finds it challenging and enjoyable to craft a
+          web app and to read someone else code.
         </p>
       </MotionDiv>
       <MotionDiv
         delay={0.1}
-        className="flex flex-col sm:flex-row gap-3 items-center justify-center mt-5 text-lg font-medium"
+        className='flex flex-col sm:flex-row gap-3 items-center justify-center mt-5 text-lg font-medium'
       >
         <Link
-          href="#contact"
-          className="bg-gray-900 group text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 hover:bg-gray-950 active:scale-100 transition"
+          href='#contact'
+          className='bg-gray-900 group text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 hover:bg-gray-950 active:scale-100 transition'
           onClick={() => {
             setIsActive('Contact');
             setTimeOfLastClick(Date.now());
           }}
         >
           Contact me here{' '}
-          <FaArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          <FaArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
         </Link>
         <a
-          href="/cv.pdf"
+          href='/cv.pdf'
           download
-          className="group bg-white text-gray-950 rounded-full px-7 py-3 flex items-center gap-2 hover:scale-110 active:scale-100 transition border border-black/10 dark:bg-white/10 dark:text-white/60"
+          className='group bg-white text-gray-950 rounded-full px-7 py-3 flex items-center gap-2 hover:scale-110 active:scale-100 transition border border-black/10 dark:bg-white/10 dark:text-white/60'
         >
           Download CV{' '}
-          <FaDownload className="opacity-70 group-hover:animate-bounce" />
+          <FaDownload className='opacity-70 group-hover:animate-bounce' />
         </a>
         <a
-          target="_blank"
-          href="https://www.linkedin.com/in/almujahid-jamion-fullstack-web-developer/"
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full active:scale-100 transition border border-black/10 hover:scale-[1.15] hover:text-gray-950 dark:bg-white/10 dark:text-white/60"
+          target='_blank'
+          href='https://www.linkedin.com/in/almujahid-jamion-fullstack-web-developer/'
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full active:scale-100 transition border border-black/10 hover:scale-[1.15] hover:text-gray-950 dark:bg-white/10 dark:text-white/60'
         >
           <FaLinkedin />
         </a>
         <a
-          href="https://github.com/firemelon96"
-          target="_blank"
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full active:scale-100 transition border border-black/10 hover:scale-[1.15] hover:text-gray-950 dark:bg-white/10 dark:text-white/60"
+          href='https://github.com/firemelon96'
+          target='_blank'
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full active:scale-100 transition border border-black/10 hover:scale-[1.15] hover:text-gray-950 dark:bg-white/10 dark:text-white/60'
         >
           <FaGithub />
         </a>
